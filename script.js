ffa62a15dd2a97a2404e7c9d4bacfb788026c9a0
VU NHU ANH <vunhuanh@gmail.com>
2017-06-29 11:09:57 -0400

    Added jquery and more logos

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $("#button1").click(function() { 
+      $("#email").toggle(500);
+  });
+  $("#button2").click(function() { 
+    $("#github").toggle(500);
+  });
+});
