094400071e9c4579f6ab0468a44f2fb975280815
VU NHU ANH <vunhuanh@gmail.com>
2017-07-02 15:38:54 -0400

    About done.

--- script.js
+++ script.js
@@ -1,16 +1,9 @@
 $(document).ready(function(){
-  /*
-  $('#main li ul').hide();
-    $('#main li').click(function() {
-        $(this).children('ul').slideToggle("slow");
-        $(this).toggleClass('open');
-  });*/
-
   $(".button").click(function() { 
     var clicked = this;
     $clicked = $(clicked);
     var target = $clicked.data("target");
-    $("#" + target).slideToggle(500);
+    $("#" + target).slideToggle(400);
 
     $(".rotate").toggle();
   });
